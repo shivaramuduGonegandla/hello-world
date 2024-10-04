a833083922ac528e0eb364ae46bfc68347623db4
shivaramudu <shivaramudu2002@gmail.com>
2024-10-04 16:55:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World!</h1>
+        <p>This is a simple React app.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
